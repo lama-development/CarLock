341536ad29ac56409d04247f2e2cd8fc188923e1
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-07-30 16:30:03 +0200

    Update versioncheck.lua

--- versioncheck.lua
+++ versioncheck.lua
@@ -6,10 +6,10 @@ function checkVersion(err,responseText, headers)
     -- Returns the current version set in fxmanifest.lua
 	curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
 
-	if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
-		print(resourceName .. " is outdated.\nLatest version: " .. responseText .. "\nCurrent version: " .. curVersion .. "\nPlease update it from https://github.com" .. updatePath)
+	if tonumber(curVersion) < tonumber(responseText) then
+		print("^3WARNING!\n" .. resourceName .. " is outdated.\nLatest version: " .. responseText .. "Current version: " .. curVersion .. "\nPlease update it from: https://github.com" .. updatePath)
 	else
-		print(resourceName .." is up to date. Enjoy.")
+		print("^5" .. resourceName .." is up to date. Enjoy.")
 	end
 end
 
