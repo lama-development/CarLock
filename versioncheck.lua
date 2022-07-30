0dfa9d911cc60037206a76113e15f1e8ef3e9843
Lama <79053058+ItzEndah@users.noreply.github.com>
2022-07-30 15:48:12 +0200

    Add files via upload

---
+++ versioncheck.lua
@@ -0,0 +1,17 @@
+Citizen.CreateThread( function()
+updatePath = "/ItzEndah/HealingPads"
+resourceName = "HealingPads"
+
+function checkVersion(err,responseText, headers)
+    -- Returns the current version set in fxmanifest.lua
+	curVersion = GetResourceMetadata(GetCurrentResourceName(), "version")
+
+	if curVersion ~= responseText and tonumber(curVersion) < tonumber(responseText) then
+		print(resourceName .. " is outdated.\nLatest version: " .. responseText .. "\nCurrent version: " .. curVersion .. "\nPlease update it from https://github.com" .. updatePath)
+	else
+		print(resourceName .." is up to date. Enjoy.")
+	end
+end
+
+PerformHttpRequest("https://raw.githubusercontent.com" .. updatePath .. "/main/version", checkVersion, "GET")
+end)
